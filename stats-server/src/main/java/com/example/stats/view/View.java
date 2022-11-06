6501b46c2c4360e6a54dae9e64cdb16eb0a9a4aa
Armen Katvalyan <aakatvalyan@bk.ru>
2022-11-06 22:53:21 +0300

    refactor: add validation on view model

--- stats-server/src/main/java/com/example/stats/view/View.java
+++ stats-server/src/main/java/com/example/stats/view/View.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter
@@ -23,12 +24,15 @@ public class View {
     private Long id;
 
     @NotNull
+    @Size(max = 100)
     private String app;
 
     @NotNull
+    @Size(max = 100)
     private String uri;
 
     @NotNull
+    @Size(max = 100)
     private String ip;
 
     private LocalDateTime timestamp;
